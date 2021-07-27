fb5f04daa23765012a1000ebc59771d0e8dbe731
jolizaro <Jolizaro@gmail.com>
2021-07-27 18:43:54 -0400

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import Home from './pages/Home'
 import Contact from './pages/Contact'
 import Portfolio from './pages/Portfolio'
