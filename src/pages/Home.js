4f193d033147299b8b41decfc483714706e146ce
jolizaro <Jolizaro@gmail.com>
2021-07-27 18:53:32 -0400

    added image folder to source

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <div className='home-page'> 
       <div className='hero'>
-        <img src='/Joliza.jpg' alt='Joliza'/>
+        <img src='../images/Joliza.jpg' alt='Joliza'/>
         <div className="hero-text">
           <h1>Hi! Im Joliza!</h1>
           <p> I am a full-stack software engineer who enjoys working with React for frontend design. I also love designing backend data structures and am pursuing a career as a freelance web developer. I'd love to hear from you regarding the needs of your small business!</p>
--- src/projectData.js
+++ src/projectData.js
@@ -3,43 +3,43 @@ const projects = [
     title: 'FashApp',
     desc: 'Yelp for the fashionista! Search our database of brands to see customer reviews and details, and add your own. Built with React, MongoDB, Express, Bootstrap, Node.js.',
     link: 'https://fashapp-ss.herokuapp.com/',
-    img: '/Fashapp_img.PNG'
+    img: '../images/Fashapp_img.PNG'
   },
   {
     title: 'Employee Directory',
     desc: 'A sortable table of employees, built using React, that can also be filtered by location.',
     link: 'https://jolizaro.github.io/employee-directory/',
-    img: '/coding-quiz.PNG'
+    img: '../images/coding-quiz.PNG'
   },
   {
     title: 'Bikr',
     desc: 'Sell and purchase bicycles from neighbors nearby. Built using Handlebars, Express, Node.js, and MySQL.',
     link: 'https://desolate-basin-02888.herokuapp.com/',
-    img: '/bikr_image2.png'
+    img: '../images/bikr_image2.png'
   },
   {
     title: 'Fitness Tracker',
     desc: 'An application that allows the user to create new workouts, add exercises to workouts and see stats for the past 7 days of workouts. Built using MongodDB and Express',
     link: 'https://fitness-tracker-joli.herokuapp.com/',
-    img: '/fitnessTracker_img.PNG'
+    img: '../images/fitnessTracker_img.PNG'
   },
   {
     title: 'Workday Scheduler',
     desc: 'Use this app to plan your daily rhythms and keep track of events. Built using HTML, CSS and jQuery.',
     link: 'https://jolizaro.github.io/third-party-API-hw/',
-    img: 'workday-scheduler.gif'
+    img: '../images/workday-scheduler.gif'
   },
   {
     title: 'PokeTeams',
     desc: 'Take a quiz to be assigned a unique team of pokemon!',
     link: 'https://juiceten.github.io/Pokemon-Battle-Theme/',
-    img: '/Pokemonproj.PNG'
+    img: '../images/Pokemonproj.PNG'
   },
   {
     title: 'JavaScript Coding Quiz',
     desc: 'Test your knowledge of JavaScript basics!',
     link: 'https://jolizaro.github.io/Javascript-Quiz-HW/',
-    img: '/coding-quiz.PNG'
+    img: '../images/coding-quiz.PNG'
   },
 ]
 
